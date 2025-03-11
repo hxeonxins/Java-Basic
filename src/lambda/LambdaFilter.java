fa7ee94d0b34a6545e2a5151dafdaacbd29360e1
심현진 <hyunjin.dev143@gmail.com>
2025-03-11 09:45:34 +0900

    study::Exceoption

--- src/lambda/LambdaFilter.java
+++ src/lambda/LambdaFilter.java
@@ -12,7 +12,11 @@ public class LambdaFilter {
     names.add("Julie"); //names에 하나씩 저장됨, 동적 영역에 저장됨
 
     List<String> names2 = Arrays.asList("John", "Jane", "Julie"); //정적으로 저장, 수정할 수 없음
-    //names2.add("sdfp"); --> error!
+    try {
+      names2.add("sdfp");
+    } catch (UnsupportedOperationException e) {
+      System.out.println("리스트가 정적으로 할당되어서 수정이 불가한 상태입니다." + e.getMessage());
+    }
     System.out.println(names2);
   }
 }
