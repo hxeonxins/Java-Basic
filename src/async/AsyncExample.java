bf5b74fe96b8f1286c44567e053a50df4467f70c
심현진 <hyunjin.dev143@gmail.com>
2025-03-11 12:31:12 +0900

    study::Async

--- src/async/AsyncExample.java
+++ src/async/AsyncExample.java
@@ -1,4 +1,29 @@
 package async;
 
+import java.util.concurrent.CompletableFuture;
+
 public class AsyncExample {
+  public static void main(String[] args) {
+
+    //동기 처리, 순서대로 실행됨
+    System.out.println("Long Time Main Thread is playing......");
+    System.out.println("Main Thread is playing...");
+
+    CompletableFuture.supplyAsync( ()->{
+      try {
+        Thread.sleep(2);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+
+      return "비동기 작업이 오랜시간 끝에 완료되었습니다.";
+    }).thenAccept(System.out::println);
+
+    System.out.println("Main Thread is finishing...");
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
--- src/async/CompletableFutureCombine.java
+++ src/async/CompletableFutureCombine.java
@@ -1,4 +1,15 @@
 package async;
 
+import java.util.concurrent.CompletableFuture;
+
 public class CompletableFutureCombine {
+  public static void main(String[] args) {
+    CompletableFuture<Integer> f1 = CompletableFuture.supplyAsync( ()->
+      10);
+    CompletableFuture<Integer> f2 = CompletableFuture.supplyAsync( ()->
+      20);
+
+    f1.thenCombine(f2, Integer::sum)//직관적으로 이해 가능한 코드, 추상적임
+            .thenAccept(System.out::println); //thenAccept가 출력함
+  }
 }
